06aa69563356b2821c4144c7072460be34003dbb
Adam Eastwood <adam-3@live.co.uk>
2025-01-07 15:27:12 +0000

    feat: reordered games to be alphabetical

--- src/utils/games.js
+++ src/utils/games.js
@@ -4,16 +4,16 @@ import FlappyBird from '../components/eastereggs/games/flappybird/Game';
 export const useGamesList = () => {
     return [
         {
-            component: RunnerGame,
-            instructions: 'Avoid the obstacles and jump to clear them',
-            controls: ['Space or Left Mouse Button - Jump'],
-            name: 'Runner'
-        },
-        {
             component: FlappyBird,
             instructions: 'You know the rules, get through the pipes without hitting them!',
             controls: ['Space or Left Mouse Button - Flap'],
             name: 'Flappy Bird'
+        },
+        {
+            component: RunnerGame,
+            instructions: 'Avoid the obstacles and jump to clear them',
+            controls: ['Space or Left Mouse Button - Jump'],
+            name: 'Runner'
         }
     ];
 };
